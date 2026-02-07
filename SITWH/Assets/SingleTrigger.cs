a3830d5b80a00a0dba95cafca6bc92167b3bd7f7
Boswell97 <165895768+Boswell97@users.noreply.github.com>
2026-02-07 15:59:28 +0100

    se pudo

---
+++ SITWH/Assets/SingleTrigger.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class SingleTrigger : MonoBehaviour
+{
+    public string requiredTag;
+    public string targetLayer = "Grabbable";
+
+    [SerializeField] private bool isActive = false;
+    private int layerValue;
+
+    void Start()
+    {
+        layerValue = LayerMask.NameToLayer(targetLayer);
+        GetComponent<Collider>().isTrigger = true;
+        Debug.Log($"✅ Trigger '{gameObject.name}' listo para tag: {requiredTag}");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.layer == layerValue)
+        {
+            if (other.CompareTag(requiredTag))
+            {
+                isActive = true;
+             
+                Destroy(other.gameObject);
+            }
+            else
+            {
+              
+                Destroy(other.gameObject);
+            }
+        }
+    }
+
+    public bool IsActive() => isActive;
+    public void ResetTrigger() => isActive = false;
+}
---
+++ SITWH/Assets/TriggerManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class TriggerManager : MonoBehaviour
+{
+    private SingleTrigger[] childTriggers;
+
+    void Start()
+    {
+        childTriggers = GetComponentsInChildren<SingleTrigger>();
+    }
+
+    public bool AreAllTriggersActive()
+    {
+        foreach (var trigger in childTriggers)
+        {
+            if (!trigger.IsActive()) return false;
+        }
+        return true;
+    }
+
+    public void ResetAllTriggers()
+    {
+        foreach (var trigger in childTriggers)
+        {
+            trigger.ResetTrigger();
+        }
+    }
+}
