fc783a230afe2640eb5970a4ce870e40ea0fd955
Boswell97 <165895768+Boswell97@users.noreply.github.com>
2026-02-24 22:13:52 +0100

    asdfghjkl

---
+++ SITWH/Assets/FMODDialoguePlayer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class FMODDialoguePlayer : MonoBehaviour
+{
+    public void PlayDialogue(EventReference dialogueEvent, string subtitle, float duration)
+    {
+        EventInstance instance = RuntimeManager.CreateInstance(dialogueEvent);
+        instance.start();
+        instance.release();
+        SubtitleManager.Instance.Show(subtitle, duration);
+    }
+}
---
+++ SITWH/Assets/SubtitleManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class SubtitleManager : MonoBehaviour
+{
+    public static SubtitleManager Instance;
+    public TextMeshProUGUI subtitleText;
+    Coroutine routine;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Show(string text, float duration)
+    {
+        if (routine != null) StopCoroutine(routine);
+        routine = StartCoroutine(ShowRoutine(text, duration));
+    }
+
+    IEnumerator ShowRoutine(string text, float duration)
+    {
+        subtitleText.text = text;
+        subtitleText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(duration);
+        subtitleText.gameObject.SetActive(false);
+    }
+}
