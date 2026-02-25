8c68f77c35da57710b282a267f138c29dbdd7977
Boswell97 <165895768+Boswell97@users.noreply.github.com>
2026-02-25 18:30:56 +0100

    promesas de beta

--- SITWH/Assets/Scenes/Scripts/CumpsLogic.cs
+++ SITWH/Assets/Scenes/Scripts/CumpsLogic.cs
@@ -4,98 +4,85 @@ using System.Collections;
 public class CrumpsLogic : MonoBehaviour
 {
     public Animator animator;
-    public Collider dodgeTrigger;
     public LayerMask grabbableLayer;
 
     public float health = 100f;
     public float healthChangeAmount = 10f;
 
-    bool claps;
-    bool ups;
-    bool dodges;
-    bool no;
-
-    Coroutine reactionRoutine;
-    Coroutine dodgeRoutine;
+    bool isReacting;
+    Coroutine currentReaction;
 
     void Start()
     {
         if (animator == null) animator = GetComponent<Animator>();
     }
 
-    void Update()
-    {
-        animator.SetBool("Claps", claps);
-        animator.SetBool("Ups", ups);
-        animator.SetBool("Dodges", dodges);
-        animator.SetBool("No", no);
-    }
-
     public void OnGoodObjectDestroyed(Vector3 position)
     {
         health += healthChangeAmount;
-
-        if (reactionRoutine != null)
-            StopCoroutine(reactionRoutine);
-
-        reactionRoutine = StartCoroutine(GoodReaction());
+        StartReaction(GoodReaction());
     }
 
     public void OnBadObjectDestroyed(Vector3 position)
     {
         health -= healthChangeAmount;
-
-        if (reactionRoutine != null)
-            StopCoroutine(reactionRoutine);
-
-        reactionRoutine = StartCoroutine(BadReaction());
-    }
-
-    IEnumerator GoodReaction()
-    {
-        ResetReactions();
-        claps = true;
-        yield return new WaitForSeconds(1f);
-        ResetReactions();
-    }
-
-    IEnumerator BadReaction()
-    {
-        ResetReactions();
-        no = true;
-        yield return new WaitForSeconds(1f);
-        ResetReactions();
-    }
-
-    IEnumerator DodgeReaction()
-    {
-        if (reactionRoutine != null) yield break;
-
-        ResetReactions();
-        dodges = true;
-        yield return new WaitForSeconds(0.7f);
-        dodges = false;
-    }
-
-    void ResetReactions()
-    {
-        claps = false;
-        ups = false;
-        no = false;
-        dodges = false;
+        StartReaction(BadReaction());
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (IsInLayerMask(other.gameObject, grabbableLayer))
         {
-            if (reactionRoutine != null) return;
-
-            if (dodgeRoutine != null)
-                StopCoroutine(dodgeRoutine);
-
-            dodgeRoutine = StartCoroutine(DodgeReaction());
+            if (!isReacting)
+                StartReaction(DodgeReaction());
         }
+    }
+
+    void StartReaction(IEnumerator reaction)
+    {
+        if (currentReaction != null)
+            StopCoroutine(currentReaction);
+
+        currentReaction = StartCoroutine(reaction);
+    }
+
+    IEnumerator GoodReaction()
+    {
+        isReacting = true;
+
+        animator.ResetTrigger("NoTrigger");
+        animator.ResetTrigger("DodgeTrigger");
+
+        animator.SetTrigger("ClapTrigger");
+
+        yield return new WaitForSeconds(1.2f);
+
+        isReacting = false;
+    }
+
+    IEnumerator BadReaction()
+    {
+        isReacting = true;
+
+        animator.ResetTrigger("ClapTrigger");
+        animator.ResetTrigger("DodgeTrigger");
+
+        animator.SetTrigger("NoTrigger");
+
+        yield return new WaitForSeconds(1.2f);
+
+        isReacting = false;
+    }
+
+    IEnumerator DodgeReaction()
+    {
+        isReacting = true;
+
+        animator.SetTrigger("DodgeTrigger");
+
+        yield return new WaitForSeconds(0.7f);
+
+        isReacting = false;
     }
 
     bool IsInLayerMask(GameObject obj, LayerMask mask)
